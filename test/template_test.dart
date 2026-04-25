84464dba50a924ae943d0653c1141b93af55defa
Журат Максим <ChessMax@yandex.ru>
2026-04-26 00:14:01 +0300

    Fix template test compilation errors.

--- test/template_test.dart
+++ test/template_test.dart
@@ -6,46 +6,42 @@ import 'package:pluto/template/template.dart';
 import 'package:test/test.dart';
 
 void main() {
-  Node parse(String source) => Parser().parse(Lexer().lex(source).toList());
+  Node parse(String source) => DocumentNode(Parser().parse(Lexer().lex(source).toList()).toList());
   String codeGen(String source) => CodeGenerator().generate(parse(source));
   Template getTemplate(String source) => Template(codeGen(source));
 
-  test('implicit expression should return correct values', () async {
+  test('implicit expression should return correct values 1', () async {
     final template = getTemplate('<p>@model</p>');
     final result = await template.render('Ivan');
-    print('result: $result');
 
     expect(result , '<p>Ivan</p>');
   });
 
-  test('implicit expression should return correct values', () async {
+  test('implicit expression should return correct values 2', () async {
     final template = getTemplate('<p>@model.name</p>');
     final result = await template.render({'name': 'Ivan'});
-    print('result: $result');
 
     expect(result , '<p>Ivan</p>');
   });
 
-  test('implicit expression should return correct values2', () async {
+  test('implicit expression should return correct values 3', () async {
     final template = getTemplate('@model.name');
     final result = await template.render({'name': 'Ivan'});
-    print('result: $result');
 
     expect(result , 'Ivan');
   });
 
 
-  test('implicit expression should return correct values', () async {
+  test('implicit expression should return correct values 4', () async {
     final template = getTemplate('<p>@DateTime.now()</p>');
     final result = await template.render(null);
-    print('result: $result');
 
     // <p>2026-04-22 22:20:51.556456</p>
     final regex = RegExp(r'^<p>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}<\/p>$');
     expect(regex.hasMatch(result) , true);
   });
 
-  test('escape should return correct values', () async {
+  test('escape should return correct values 5', () async {
     final template = getTemplate('<p>@@model</p>');
     final result = await template.render(null);
     print('result: $result');
