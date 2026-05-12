7abc594f53a530b82c36d4ef864778f424012d27
Журат Максим <ChessMax@yandex.ru>
2026-05-12 21:39:06 +0300

    Pluto dependency added.

--- lib/commands/init_course_command.dart
+++ lib/commands/init_course_command.dart
@@ -7,9 +7,11 @@ class InitCourseCommand extends Command {
   const InitCourseCommand() : super('init');
 
   Future<void> execute(String name) async {
-    String relativePath(String part, [String? part2]) => join('.', name, part, part2);
+    String relativePath(String part, [String? part2]) =>
+        join('.', name, part, part2);
 
     await run(['dart', 'create', name]);
+    await run(['dart', 'pub', 'add', 'dev:pluto:{path: ..}'], workingDirectory: relativePath(''));
     await deleteDirectory(relativePath('bin'));
     await deleteDirectory(relativePath('test'));
     await deleteFile(relativePath('lib', '$name.dart'));
@@ -26,12 +28,13 @@ class InitCourseCommand extends Command {
     print('Deleting dir `$path`...');
     await Directory(path).delete(recursive: true);
   }
-  
-  Future<void> run(List<String> args) async {
+
+  Future<void> run(List<String> args, {String? workingDirectory}) async {
     final command = args.join(' ');
-    print('Run `$command`...');
-    final result = await Process.run(args.removeAt(0), args);
+    print('Run `$command${workingDirectory != null ? ' at `$workingDirectory`' : ''}`...');
+    final result = await Process.run(args.removeAt(0), args, workingDirectory: workingDirectory);
     if (result.exitCode != 0) {
+      print(result.stderr);
       throw 'Command `command` failed with error code: ${result.exitCode}';
     }
   }
